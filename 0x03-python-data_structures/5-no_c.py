523aa2dc78de61c99d75c408c34d602eadd30c85
Abel-G <abeljeb@gmail.com>
2023-01-09 12:30:45 -0800

    print

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 def no_c(my_string):
     for i in my_string:
         if i == 'c' or i == 'C':
-            my_string.replace(i,'')
+            my_string.replace(i, '')
             return my_string
