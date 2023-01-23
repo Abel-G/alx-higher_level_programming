415e1cc98f149afebe666d9892676c2e4b7c7af4
Abel-G <abeljeb@gmail.com>
2023-01-23 07:51:23 -0800

    print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    a = 0
+    b = 0
+    for i in my_list:
+        a += 1
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+            b += 1
+        except IndexError:
+            break
+    print("")
+    return b
