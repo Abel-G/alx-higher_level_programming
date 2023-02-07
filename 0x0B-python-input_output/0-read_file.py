caa136194c787eefdab4618dcdbb851aa62825a5
Abel-G <abeljeb@gmail.com>
2023-02-07 09:44:06 -0800

    print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-        """read_file reads a text file (UTF8) and prints it to stdout """
-        with open(filename, 'r', encoding='utf-8') as f:
-                print(f.read(), end="")
+    """read_file reads a text file (UTF8) and prints it to stdout """
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
