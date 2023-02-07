9ee972e96ea1f360da2a525bcef9bd9c66c84f11
Abel-G <abeljeb@gmail.com>
2023-02-07 12:14:07 -0800

    print

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """ contan write file function """
 
 
-def write_file(filename="", text=""):    
+def write_file(filename="", text=""):
     """ writes a string to a text file """
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
