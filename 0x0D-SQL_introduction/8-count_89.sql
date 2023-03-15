482b0184165e99611e67f038390858d7858b8593
Abel-G <abeljeb@gmail.com>
2023-03-15 06:48:36 -0700

    push

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,3 @@
---a script that displays the number of records with id = 89 in the table
-SELECT COUNT(*)
-FROM first_table
-WHERE id = 89;
+#!/usr/bin/env bash
+-- script that displays the number of records with id = 89 in the table
+SELECT COUNT(*) FROM `first_table` WHERE `id`= 89;
