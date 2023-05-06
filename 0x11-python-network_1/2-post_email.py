5e0beafe3e4e24e06807011a8ecea027c5a3fd3b
Abel-G <abeljeb@gmail.com>
2023-05-06 10:05:01 -0700

    print

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email, sends a POST request"""
+import urllib.request
+import urllib.parse
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+with urllib.request.urlopen(url, data) as f:
+    response = f.read().decode('utf-8')
+print(response)
