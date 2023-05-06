2fb845076615a89d4d0371d186933912440f8061
Abel-G <abeljeb@gmail.com>
2023-05-06 09:39:37 -0700

    print

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 
 import urllib.request
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+    with request.urlopen('https://alx-intranet.hbtn.io/status')\
          as response:
         result = response.read()
         print("Body response:")
