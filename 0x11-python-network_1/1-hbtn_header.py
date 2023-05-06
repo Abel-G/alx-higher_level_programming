657c0dc4c49a6a71bfa8f2b7516f8d69f550227d
Abel-G <abeljeb@gmail.com>
2023-05-06 09:58:52 -0700

    print

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ script that takes in a URL, sends request to URL  """
 import sys
-from urllib import request
+import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.info().get('X-Request-Id'))
